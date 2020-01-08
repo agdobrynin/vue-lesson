242a465f4f67954a354185fc2a300354d4b66ac5
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-08 17:02:24 +0400

    fear(component): Add component "Car"

---
+++ src/Car.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ carName }}</h1>
+        <p>{{ carYear }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Car",
+        data() {
+            return {
+                carName: "Ford",
+                carYear: 2015,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    h1 {color: blue;}
+    p {color: gray;}
+</style>
